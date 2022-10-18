9d9f638f0082b4788c1313f0792ebd68472f614e
Pete Fowler <fowler.pete@gmail.com>
2022-10-18 14:58:40 -0600

    Add membership macros & validations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,2 +1,7 @@
 class Membership < ApplicationRecord
+  belongs_to :client
+  belongs_to :gym
+  validates :gym_id, presence: true
+  validates :client_id, presence: true
+  validates :charge, presence: true, numericality: {greater_than: 0, less_than: 9999}
 end
