caeee4763bcb101e471def7f05296220744c4399
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 10:56:54 -0600

    Add memberships sum to show client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,12 @@
 class ClientsController < ApplicationController
+  def show
+    client = Client.find(params[:id])
+    # byebug
+    sum = client.memberships.sum(:charge)
+    render json: {
+      name: client.name, 
+      age: client.age, 
+      membership_total: sum
+      }
+  end
 end
